3eecd529e63b62ada7730b6c34269b54bb224caf
elviscastilho <141188599+elviscastilho@users.noreply.github.com>
2023-09-03 13:39:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function imprimirLetra() {
+    const frases = [
+      "Um elefante incomoda muita gente,",
+      "Dois elefantes incomodam, incomodam muito mais,",
+      "Três elefantes incomodam muita gente,",
+      "Quatro elefantes incomodam, incomodam muito mais,",
+      "Cinco elefantes incomodam muita gente,",
+      "Seis elefantes incomodam, incomodam muito mais,",
+      "Sete elefantes incomodam muita gente,",
+      "Oito elefantes incomodam, incomodam muito mais,",
+      "Nove elefantes incomodam muita gente,",
+      "Dez elefantes incomodam, incomodam muito mais."
+    ];
+  
+    for (let i = 0; i < frases.length; i++) {
+      console.log(frases[i]);
+    }
+  }
+  
+  imprimirLetra();
+  
